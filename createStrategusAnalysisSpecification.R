b8946f622073c78e7c2e5be55fadb48d47c87ac3
JaehyeongCho <boyinai03@gmail.com>
2023-11-16 12:48:07 +0900

    Modify strategus and conceptsetIds

--- createStrategusAnalysisSpecification.R
+++ createStrategusAnalysisSpecification.R
@@ -397,33 +397,33 @@ tcis <- list(
 )
 #
 # # TEST ONLY
- tcis <- list(
-   # Study population: replication study 12 (ARISTOTLE trial)
-   list(
-     targetId = 346,#1787750, # Apixaban (ARISTOTLE trial)
-     comparatorId = 347,#1787751, # Warfarin (ARISTOTLE trial)
-     #indicationId = , #
-     #genderConceptIds = c(8507, 8532), # use valid genders (remove unknown)
-     #minAge = 35, # All ages In years. Can be NULL
-     #maxAge = NULL, # All ages In years. Can be NULL
-     excludedCovariateConceptIds = c(
-       43013024,# Apixaban
-       1310149 # Warfarin
-     )
-   ),
-   list(
-     targetId = 373,#1787752, # Apixaban (ARISTOTLE trial indication)
-     comparatorId = 374,#1787753, # Warfarin (ARISTOTLE trial indication)
-     #indicationId = , #
-     #genderConceptIds = c(8507, 8532), # use valid genders (remove unknown)
-     #minAge = 35, # All ages In years. Can be NULL
-     #maxAge = NULL, # All ages In years. Can be NULL
-     excludedCovariateConceptIds = c(
-       43013024,# Apixaban
-       1310149 # Warfarin
-     )
-   )
- )
+ # tcis <- list(
+ #   # Study population: replication study 12 (ARISTOTLE trial)
+ #   list(
+ #     targetId = 346,#1787750, # Apixaban (ARISTOTLE trial)
+ #     comparatorId = 347,#1787751, # Warfarin (ARISTOTLE trial)
+ #     #indicationId = , #
+ #     #genderConceptIds = c(8507, 8532), # use valid genders (remove unknown)
+ #     #minAge = 35, # All ages In years. Can be NULL
+ #     #maxAge = NULL, # All ages In years. Can be NULL
+ #     excludedCovariateConceptIds = c(
+ #       43013024,# Apixaban
+ #       1310149 # Warfarin
+ #     )
+ #   ),
+ #   list(
+ #     targetId = 373,#1787752, # Apixaban (ARISTOTLE trial indication)
+ #     comparatorId = 374,#1787753, # Warfarin (ARISTOTLE trial indication)
+ #     #indicationId = , #
+ #     #genderConceptIds = c(8507, 8532), # use valid genders (remove unknown)
+ #     #minAge = 35, # All ages In years. Can be NULL
+ #     #maxAge = NULL, # All ages In years. Can be NULL
+ #     excludedCovariateConceptIds = c(
+ #       43013024,# Apixaban
+ #       1310149 # Warfarin
+ #     )
+ #   )
+ # )
 ########### Cohort: O ##############
 outcomes <- tibble(
   cohortId = c(9, 10, 11, 12), # 3P MACE, 4P MACE, HHF + CV death, Storke + systemic embolism
@@ -552,7 +552,7 @@ cohortDiagnosticsModuleSpecifications <- createCohortDiagnosticsModuleSpecificat
   runBreakdownIndexEvents = TRUE,
   runIncidenceRate = TRUE,
   runCohortRelationship = TRUE,
-  runTemporalCohortCharacterization = TRUE,
+  runTemporalCohortCharacterization = FALSE,
   minCharacterizationMean = 0.0001,
   temporalCovariateSettings = getDefaultCovariateSettings(),
   incremental = FALSE,
@@ -699,4 +699,4 @@ if (!dir.exists(outputFolder)) {
   dir.create(outputFolder, recursive = TRUE)
 }
 
-ParallelLogger::saveSettingsToJson(analysisSpecifications, file.path(outputFolder, "analysisSpecificationTest.json"))
+ParallelLogger::saveSettingsToJson(analysisSpecifications, file.path(outputFolder, "analysisSpecification.json"))
